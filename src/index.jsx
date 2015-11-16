444495cc9a7395630e1ccddb221bf4b8cb0eac85
Scott Langeberg <slangeberg@gmail.com>
2015-11-16 14:10:35 -0600

    Came to realize main index.jsx file is not subject to hot reload

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Voting from './components/Voting';
 
-const pair = ['Trainspotting', '28 Days Later'];
+const pair = ['Trairnsporting', '28 Days Later'];
 
 ReactDOM.render(
     <Voting pair={pair} />,
